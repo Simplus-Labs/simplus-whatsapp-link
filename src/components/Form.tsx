fe15fc0a4c235f967e4b46345de28fcd0d14f441
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-09-24 21:15:35 -0500

    feat: ✨ Emoji

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,19 +4,22 @@ import { Button } from "@/components/ui/button"
 import { PhoneInput } from 'react-international-phone';
 import 'react-international-phone/style.css';
 
-const inputStyle = "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 
 function Form() {
   return (
     <div className="flex flex-col gap-4 w-1/2 p-7 border rounded-sm shadow-lg">
-      <Label>Type your WhatsApp phone number</Label>
-      <PhoneInput
-        defaultCountry="us"
-        inputClassName={inputStyle}
-      />
-
-      <Label htmlFor="">Custom Message</Label>
-      <Textarea></Textarea>
+      <div>
+        <Label>Type your WhatsApp phone number</Label>
+        <PhoneInput
+          defaultCountry="us"
+          inputClassName="w-full"
+        />
+      </div>
+      <div>
+        <Label htmlFor="">Custom Message</Label>
+        <div className=" w-10 h-10 p-2 border flex items-center justify-center rounded-full mb-3 cursor-pointer hover:bg-slate-200">🙂</div>
+        <Textarea></Textarea>
+      </div>
 
       <Button>Generate</Button>
     </div>
