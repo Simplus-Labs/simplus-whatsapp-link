4fc1b26fc25d2deac037a284648fe1ad755bff85
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-09-24 21:26:37 -0500

    feat: ✨ add message layaut

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,12 +1,12 @@
 
 function Preview() {
     return (
-        <div className="flex flex-col gap-4 w-1/2 border rounded-sm shadow-lg">
-            <div className="w-full flex items-center h-12 bg-gray-300 gap-5 p-1">
+        <div className="flex flex-col gap-4 w-1/2 border rounded-sm shadow-lg justify-between">
+            <div className="w-full flex items-center h-12 bg-slate-200 gap-5 p-1">
                 <img className=" w-10 h-10 rounded-full p-1" src="https://avatars.githubusercontent.com/u/70488358?v=4" alt="image placeholder" />
                 <p className=" text-sm">+1 03840328038</p>
             </div>
-            <div className=""></div>
+            <div className=" bg-[#e0fec5] w-3/4 h-fit rounded-sm self-end mb-10 mr-5 p-2">something dsfdfds dsf df dsf dsf dsf dsfds fds fsadosa dassadksl sadasdsasa sdasdsa sadasdsa dsad asd asd sad sad asd as</div>
         </div>
     )
 }
