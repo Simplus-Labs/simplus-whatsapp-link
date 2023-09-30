a273c8098fa6cf6444dcdd09d116ea78b8719130
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-09-29 22:34:32 -0500

    fix: 🐛 add the avatar ui component

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,11 +1,16 @@
 import { MessageContext } from "@/contexts/MessageContext"
 import { useContext } from "react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 function Preview() {
     const { message } = useContext(MessageContext)
     return (
         <div className="flex flex-col gap-4 w-1/2 border rounded-sm shadow-lg justify-between">
             <div className="w-full flex items-center h-12 bg-slate-200 gap-5 p-1">
-                <img className=" w-10 h-10 rounded-full p-1" src="https://avatars.githubusercontent.com/u/70488358?v=4" alt="image placeholder" />
+                <Avatar>
+                    <AvatarImage src="" />
+                    <AvatarFallback>SL</AvatarFallback>
+                </Avatar>
                 <p className=" text-sm">{message.phone}</p>
             </div>
             <div className=" bg-[#e0fec5] w-3/4 h-fit rounded-sm self-end mb-10 mr-5 p-2">{message.text}</div>
