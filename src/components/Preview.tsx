db181365940e39bc159f3413b4f5cf4e86a17cb9
Aeternus185 <andryp_@hotmail.com>
2023-10-10 21:42:18 -0400

    style: 🍱 add user icon for preview component

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,21 +1,21 @@
-import { MessageContext } from "@/contexts/MessageContext"
-import { useContext } from "react"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { MessageContext } from '@/contexts/MessageContext';
+import { useContext } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
-function Preview() {
-    const { message } = useContext(MessageContext)
-    return (
-        <div className="w-full flex flex-col gap-4 md:w-1/2  border rounded-sm shadow-lg justify-between">
-            <div className="w-full flex items-center h-12 bg-slate-200 gap-5 p-1">
-                <Avatar>
-                    <AvatarImage src="" />
-                    <AvatarFallback>SL</AvatarFallback>
-                </Avatar>
-                <p className=" text-sm">{message.phone}</p>
-            </div>
-            <div className=" bg-[#e0fec5] w-3/4 h-fit rounded-sm self-end mb-10 mr-5 p-2">{message.text}</div>
-        </div>
-    )
+function Preview(): JSX.Element {
+  const { message } = useContext(MessageContext);
+  return (
+    <div className="w-full flex flex-col gap-4 md:w-1/2  border rounded-sm shadow-lg justify-between">
+      <div className="w-full flex items-center h-12 bg-slate-200 gap-5 p-1">
+        <Avatar>
+          <AvatarImage src="/src/assets/user.svg" />
+          <AvatarFallback>SL</AvatarFallback>
+        </Avatar>
+        <p className=" text-sm">{message.phone}</p>
+      </div>
+      <div className=" bg-[#e0fec5] w-3/4 h-fit rounded-sm self-end mb-10 mr-5 p-2">{message.text}</div>
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
