ee50a24b1f0dbd5388344befa09941e53ebb002c
Aeternus185 <andryp_@hotmail.com>
2023-10-12 18:09:35 -0400

    style: 💄 enhance Preview component ui

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,19 +1,34 @@
 import { MessageContext } from '@/contexts/MessageContext';
 import { useContext } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { PaperPlaneRight } from '@phosphor-icons/react';
+import '../styles/Preview.css';
 
 function Preview(): JSX.Element {
   const { message } = useContext(MessageContext);
   return (
-    <div className="w-full flex flex-col gap-4 md:w-1/2  border rounded-sm shadow-lg justify-between">
-      <div className="w-full flex items-center h-12 bg-slate-200 gap-5 p-1">
-        <Avatar>
-          <AvatarImage src="/src/assets/user.svg" />
-          <AvatarFallback>SL</AvatarFallback>
-        </Avatar>
-        <p className=" text-sm">{message.phone}</p>
+    <div className="w-full flex flex-col gap-4 md:w-1/3 border rounded-md shadow-lg bg-white px-2 py-8">
+      <div className="w-full h-full rounded-md flex flex-col justify-between">
+        <div className="w-full rounded-t-md flex items-center h-14 bg-[#F0F2F5] gap-4 py-2 px-3">
+          <Avatar>
+            <AvatarImage src="/src/assets/user.svg" />
+            <AvatarFallback>SL</AvatarFallback>
+          </Avatar>
+          <div>{message.phone}</div>
+        </div>
+        <div
+          className="bg-[#EFEAE2] w-full h-[336px] flex flex-col items-end overflow-y-auto scrollbar"
+          style={{ backgroundImage: 'url(/src/assets/bg-whatsapp.png)' }}
+        >
+          <div className=" bg-[#D9FDD3] w-fit min-w-24 min-h-8 h-fit my-2 mr-2 ml-8 rounded-sm px-2 py-1">
+            {message.text}
+          </div>
+        </div>
+        <div className="w-full bg-[#F0F2F5] h-12 justify-center items-center flex gap-2 p-2">
+          <div className="bg-white w-full h-8 rounded-md"></div>
+          <PaperPlaneRight size={28} className="text-[#54656f]" />
+        </div>
       </div>
-      <div className=" bg-[#e0fec5] w-3/4 h-fit rounded-sm self-end mb-10 mr-5 p-2">{message.text}</div>
     </div>
   );
 }
